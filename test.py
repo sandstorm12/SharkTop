3fb5391e6bab709b4150a086b7a7ab6c9c1e7693
hamid <sandstormeatwo@gmail.com>
2021-07-06 04:22:12 +0430

    1. A first working version is implemented, now it just need polish and be installable using pip systemwide

--- test.py
+++ test.py
@@ -2,28 +2,37 @@
 """
 
 import os
+import re
 import sys
 import time
+import signal
 import curses
 import threading
+import subprocess
 
 
 PROP_REFRESH_RATE = 2
 
 start_time = None
-values = {
-    'videotestsrc': 0,
-    'fakesink': 0
-}
+close = False
+pipeline = None
+framerate = {}
 
-def _update_values(values, refresh_rate=2):
-    while True:
-        for key in values:
-            values[key] += 1
+def _update_values(process, values, refresh_rate=2):
+    for line in iter(process.stdout.readline, ""):
+        search = re.search(
+            "(.*)(framerate)(.*)(pad=\(string\))(.*)(,)(.*)(fps=\(uint\))(\d+)",
+            line.decode("utf-8") 
+        )
 
-        time.sleep(1 / refresh_rate)
+        if search is not None:
+            if search.group(2) == "framerate":
+                framerate[search.group(5)] = search.group(9)
+    
 
 def draw_menu(stdscr):
+    global close
+
     k = 0
     cursor_x = 0
     cursor_y = 0
@@ -58,31 +67,31 @@ def draw_menu(stdscr):
         # Render title
         stdscr.attron(curses.color_pair(4))
         stdscr.addstr(0, 0, "Pipeline:")
-        stdscr.addstr(0, 9, " gst-launch-1.0 videotestsrc ! fakesink")
+        stdscr.addstr(0, 9, " " + " ".join(pipeline))
         stdscr.attroff(curses.color_pair(4))
 
-        # Render title
-        stdscr.attron(curses.color_pair(4))
-        stdscr.addstr(1, 0, "Runtime:")
-        stdscr.addstr(1, 8, "{:.3f}".format(time.time() - start_time))
-        stdscr.attroff(curses.color_pair(4))
+        # # Render title
+        # stdscr.attron(curses.color_pair(4))
+        # stdscr.addstr(1, 0, "Runtime:")
+        # stdscr.addstr(1, 8, "{:.3f}".format(time.time() - start_time))
+        # stdscr.attroff(curses.color_pair(4))
 
         # Render headers
         stdscr.attron(curses.color_pair(1))
-        stdscr.addstr(2, 0, "Names" + (" " * (width // 2 - 5)))
-        stdscr.addstr(2, width // 2, "Values" + (" " * (width // 2 - 6)))
+        stdscr.addstr(2, 0, "Element" + (" " * (width // 2 - 7)))
+        stdscr.addstr(2, width // 2, "Framerate" + (" " * (width // 2 - 9)))
         stdscr.attroff(curses.color_pair(1))
 
         # Render values
         stdscr.attron(curses.color_pair(2))
-        for i, key in enumerate(values.keys()):
+        for i, key in enumerate(framerate.keys()):
             stdscr.addstr(3 + i, 0, key)
-            stdscr.addstr(3 + i, width // 2, str(values[key]))
+            stdscr.addstr(3 + i, width // 2, str(framerate[key]))
         stdscr.attroff(curses.color_pair(2))
         
         # Render status bar
-        statusbarstr = "Press 'q' to exit | STATUS BAR | Pos: {}, {}".format(
-            cursor_x, cursor_y
+        statusbarstr = "Press 'q' to exit | runtime: {:.2f}s | Pos: {}, {}".format(
+            time.time() - start_time, cursor_x, cursor_y
         )
         stdscr.attron(curses.color_pair(3))
         stdscr.addstr(height-1, 0, statusbarstr)
@@ -100,17 +109,49 @@ def draw_menu(stdscr):
         k = stdscr.getch()
         time.sleep(min(1 / PROP_REFRESH_RATE, .1))
 
+    close = True
 
-def _initialize_update_thread():
+
+def _initialize_update_thread(process):
     thread = threading.Thread(
-        target=_update_values, args=(values, PROP_REFRESH_RATE)
+        target=_update_values,
+        args=(process, framerate, PROP_REFRESH_RATE)
     )
     thread.daemon = True
     thread.start()
 
-if __name__ == "__main__":
-    _initialize_update_thread()
 
+def _run_command():
+    global start_time
+    global pipeline
+
+    env = os.environ.copy()
+    env["GST_DEBUG"] = "GST_TRACER:7"
+    env["GST_TRACERS"] = "framerate"
+    pipeline = [sys.argv[_] for _ in range(1, len(sys.argv))]
+    process = subprocess.Popen(
+        pipeline, stdout=subprocess.PIPE, env=env,
+        stderr=subprocess.STDOUT
+    )
+    
     start_time = time.time()
 
+    return process
+
+
+def kill(proc_pid):
+    process = psutil.Process(proc_pid)
+    for proc in process.children(recursive=True):
+        proc.kill()
+    process.kill()
+
+
+if __name__ == "__main__":
+    process = _run_command()
+
+    _initialize_update_thread(process)
+
     curses.wrapper(draw_menu)
+    # time.sleep(10)
+
+    process.kill()
